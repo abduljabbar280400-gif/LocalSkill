85323016dc172e6319c72b4653773782fd59e06a
abduljabbar2804 <abduljabbarabcd4@gmail.com>
2026-04-03 00:01:45 +0530

    added withCredentials in api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // Create Axios Instance
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
   // timeout: 10000,
   headers: {
     "Content-Type": "application/json",
