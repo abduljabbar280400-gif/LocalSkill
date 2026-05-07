5d885d767fd015dae2abf5a620a783abd9340be2
abduljabbar2804 <abduljabbarabcd4@gmail.com>
2026-05-07 19:33:14 +0530

    update in login in fix

--- Backend/bootstrap/app.php
+++ Backend/bootstrap/app.php
@@ -28,6 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
 
+        $middleware->trustProxies(at: '*');
         $middleware->statefulApi();
 
         // Named middleware aliases
